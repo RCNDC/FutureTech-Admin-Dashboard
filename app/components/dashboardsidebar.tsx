06d98498f5464b44ed41783d0cd9587c243eda3d
mikek1337 <mikekegnlyew@gmail.com>
2025-09-19 02:22:25 +0300

    fix: updated route endpoints

--- app/components/dashboardsidebar.tsx
+++ app/components/dashboardsidebar.tsx
@@ -2,55 +2,6 @@ import { Check, FormInput, GaugeCircle, Ticket, User, Users } from "lucide-react
 import { Link } from "react-router";
 
 const DashboardSideBar = () => {
-  return (
-    <div className="w-56 space-y-4 max-w-56 bg-linear-150 from-purple-900 to-purple-500 h-[91vh] p-5">
-      <div className="mt-10 space-y-2">
-        <h5 className="text-gray-300">Overview</h5>
-        <ul className="my-3 ml-2">
-          <li className=" text-white text-sm">
-            <Link to="/dashboard/home" className="flex items-center gap-2">
-              <GaugeCircle className="w-4 h-4" />
-              Dashboard
-            </Link>
-          </li>
-        </ul>
-        <h5 className="text-gray-300">Participants</h5>
-        <ul>
-          <li className="flex items-center gap-2 text-white text-sm">
-            <User className="w-5 h-5" />
-            Embassy Delegates
-          </li>
-        </ul>
-        <h5 className="text-gray-300">Event Manage</h5>
-        <ul className="my-3 ml-2 space-y-2">
-          <li className="flex items-center gap-2 text-white text-sm">
-            <Ticket className="w-4 h-4" />
-            Tickets
-          </li>
-          <li className="flex items-center gap-2 text-white text-sm">
-            <Link to="/dashboard/attendees" className="flex items-center gap-2">
-              <Users className="w-4 h-4" />
-              Attendees
-            </Link>
-          </li>
-          <li className=" text-white text-sm">
-            <Link to="/dashboard/checkin" className="flex items-center gap-2">
-              <Check className="w-4 h-4" />
-              Check-in
-            </Link>
-          </li>
-
-          <li className=" text-white text-sm">
-            <Link to="/dashboard/users" className="flex items-center gap-2">
-              <Users className="w-4 h-4" />
-              Users
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-};
     return (
         <div className="md:w-56 space-y-4 max-w-56 bg-linear-150 from-purple-900 to-purple-500 md:h-[91vh] h-screen p-5 shadow-md  relative  z-99999">
             <div className="mt-10 space-y-2">
