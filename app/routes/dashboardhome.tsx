1024e863c2b68b0e0b8c8f4ce02b3e8b7e507a99
mikek1337 <mikekegnlyew@gmail.com>
2025-09-19 01:59:01 +0300

    feat: add StatCard and TotalStat components for dashboard statistics

--- app/routes/dashboardhome.tsx
+++ app/routes/dashboardhome.tsx
@@ -1,3 +1,4 @@
+import TotalStat from "@/components/totalstats";
 import type { Route } from "../+types/root";
 
 export function meta({}: Route.MetaArgs) {
@@ -14,7 +15,7 @@ export function loader(){
 export default function DashboardHome(){
     return(
         <div>
-            
+            <TotalStat/>
         </div>
     )
 
