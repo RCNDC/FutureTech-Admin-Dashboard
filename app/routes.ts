29a50704e779bdd1ca139d59d34e715982bcf9ae
eyu-droid <beyob7543@gmail.com>
2025-09-22 16:00:20 +0300

    route

--- app/routes.ts
+++ app/routes.ts
@@ -7,36 +7,20 @@ export default [
     route("/forgot-password", "routes/forgetpassword.tsx"),
     route("/reset-password/:param", "routes/resetpassword.tsx"),
   ]),
-  layout("layouts/dashboard.tsx", [
-    route("/dashboard/home", "routes/dashboardhome.tsx"),
-    route("/dashboard/embassy", "routes/embassy/index.tsx"),
-    route("/dashboard/checkin", "routes/checkin/index.tsx"),
-    route("/dashboard/attendees", "routes/attendees/index.tsx"),
-    route("/dashboard/users", "routes/users/index.tsx"),
+
+  layout('layouts/dashboard.tsx', [
+      route('/dashboard/home', 'routes/dashboardhome.tsx'),
+      route('/dashboard/checkin', 'routes/checkin/index.tsx'),
+      route('/dashboard/submission/ngo', 'routes/submissions/ngo/index.tsx'),
+      route('/dashboard/submission/localcompany', 'routes/submissions/localcompany/index.tsx'),
+      route('/dashboard/submission/internationalcompany', 'routes/submissions/internationalcompany/index.tsx'),
+      route('/dashboard/submission/embassies', 'routes/submissions/embassy/index.tsx'),
+      route('/dashboard/submission/startups', 'routes/submissions/startup/index.tsx'),
+      route("/dashboard/attendees", "routes/attendees/index.tsx"),
+      route("/dashboard/users", "routes/users/index.tsx"),
   ]),
   layout("layouts/checkout.tsx", [
     route("checkout", "routes/checkout.tsx"),
     route("checkout/completed", "routes/completed.tsx"),
   ]),
 ] satisfies RouteConfig;
-    index("routes/home.tsx"),
-    layout('layouts/auth.tsx', [
-        route("/login", "routes/login.tsx"),
-        route("/forgot-password", "routes/forgetpassword.tsx"),
-        route('/reset-password/:param', 'routes/resetpassword.tsx')
-    ]),
-    layout('layouts/dashboard.tsx', [
-        route('/dashboard/home', 'routes/dashboardhome.tsx'),
-        route('/dashboard/checkin', 'routes/checkin/index.tsx'),
-        route('/dashboard/submission/ngo', 'routes/submissions/ngo/index.tsx'),
-        route('/dashboard/submission/localcompany', 'routes/submissions/localcompany/index.tsx'),
-        route('/dashboard/submission/internationalcompany', 'routes/submissions/internationalcompany/index.tsx'),
-        route('/dashboard/submission/embassies', 'routes/submissions/embassy/index.tsx'),
-        route('/dashboard/submission/startups', 'routes/submissions/startup/index.tsx'),
-        route("/dashboard/attendees", "routes/attendees/index.tsx"),
-    ]),
-    layout('layouts/checkout.tsx', [
-        route('checkout', 'routes/checkout.tsx'),
-        route('checkout/completed', 'routes/completed.tsx'),
-
-    ])] satisfies RouteConfig;
