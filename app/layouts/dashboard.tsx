1c79844ba8a5f7b988246a85b2bf3a6a943f7ada
eyu-droid <beyob7543@gmail.com>
2025-10-01 11:54:58 +0300

    Sidebar

--- app/layouts/dashboard.tsx
+++ app/layouts/dashboard.tsx
@@ -10,10 +10,10 @@ export default function DashboardLayout() {
       <div className="">
        {/* header component goes hear */}
         <div className="flex h-screen overflow-hidden gap-10 ">
-          
+
             <MobileSideMenu/>
-            <DashboardSideBar />
-          
+          {/* (<DashboardSideBar /> */}
+
           <div className="w-full my-10 md:max-w-[80%] overflow-y-auto">
             <Outlet />
           </div>
